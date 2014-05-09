feeca0c91e9bc8613ef93a29ffa030a406a977f4
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-05-09 22:29:25 +0200

    core: arm: Use -O3 optimization inside TARGET_arm_CFLAGS
    Change-Id: I224b62b3dada874d00a90258a1dcffcea09bc029
    
    Conflicts:
    	core/combo/TARGET_linux-arm.mk

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -68,12 +68,12 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-TARGET_arm_CFLAGS :=	-O3 \
-			-fno-inline-functions \
-			-fno-tree-vectorize \
- 			-fomit-frame-pointer \
-			-fstrict-aliasing    \
-			-funswitch-loops
+TARGET_arm_CFLAGS :=    -O3 \
+                        -fno-inline-functions \
+                        -fno-tree-vectorize \
+                        -fomit-frame-pointer \
+                        -fstrict-aliasing    \
+                        -funswitch-loops
 
 # Modules can choose to compile some source as thumb.
 TARGET_thumb_CFLAGS :=  -mthumb \
